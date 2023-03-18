b4c74fabe236f6aa95d4fa9466bd9241842daea6
Bruno da Silva Cunha <bruno.cunha2@external.stellantis.com>
2023-03-18 19:41:30 -0300

    Ex 2 - Criar um contexto com provider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,30 @@
-import Navbar from './components/Navbar';
-import PageWrapper from './components/PageWrapper';
-import Footer from './components/Footer';
-
-import HomePage from './pages/HomePage';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import PageWrapper from "./components/PageWrapper";
+import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import Context from "./context/Context";
 
 function App() {
+  const [name, setName] = useState("");
+  const [isAdmin, setIsAdmin] = useState(false);
+
+  const clearInfo = () => {
+    setName("");
+    setIsAdmin(false);
+  }
+
   return (
     <>
-      <Navbar />
-
-      <PageWrapper>
-        <HomePage />
-      </PageWrapper>
-
-      <Footer />
+      <Context.Provider
+        value={{ name: [name, setName], isAdmin: [isAdmin, setIsAdmin], clearInfo: [clearInfo] }}
+      >
+        <Navbar />
+        <PageWrapper>
+          <HomePage />
+        </PageWrapper>
+        <Footer />
+      </Context.Provider>
     </>
   );
 }
